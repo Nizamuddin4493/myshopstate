36cc14abf98ba96107da30621bd586b598393328
Nizamuddin4493 <nizamuddin4493@gmail.com>
2021-11-26 01:06:18 +0430

    set images directory in js file

--- js/script.js
+++ js/script.js
@@ -21,37 +21,37 @@ const topProducts = document.querySelector('.top-products');
 
 const topRankArr = [
   {
-    picture: '../images/product1.png',
+    picture: './images/product1.png',
     title: 'i12 pro max 6.7 inch',
     price: 'AFG 3000 - AFG 5500',
     description: "hight definition face recognistion; no accounts or sign-ups required. has been the industry's standard",
   },
   {
-    picture: '../images/product2.png',
+    picture: './images/product2.png',
     title: '2021 mobile phone with good services',
     price: 'AFG 2500 - AFG 4900',
     description: "mobile phone with good services; no accounts or sign-ups required. has been the industry's standard",
   },
   {
-    picture: '../images/product3.png',
+    picture: './images/product3.png',
     title: 'Samsung S21 Smartphone Android',
     price: 'AFG 7189',
     description: "Samsung Smartphone Android; no accounts or sign-ups required. has been the industry's standard",
   },
   {
-    picture: '../images/product4.png',
+    picture: './images/product4.png',
     title: '2021 Direct sell by company',
     price: 'AFG 2800 - AFG 3700',
     description: "Direct sell by company; no accounts or sign-ups required. has been the industry's standard",
   },
   {
-    picture: '../images/product5.png',
+    picture: './images/product5.png',
     title: 'M11 ultra smartphone',
     price: 'AFG 3700 - AFG 5380',
     description: "ultra smartphone; no accounts or sign-ups required. has been the industry's standard",
   },
   {
-    picture: '../images/product6.png',
+    picture: './images/product6.png',
     title: 'cheap price Android 10',
     price: 'AFG 1050 - AFG 3200',
     description: "cheap price Android; no accounts or sign-ups required. has been the industry's standard",
